d4133810362c34be047d4e787c9d11a7bd52a58c
Diana Carolina Badas <33879699+DianaBaRo@users.noreply.github.com>
2020-02-04 17:11:49 +0000

    Add solution

---
+++ solution.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the timeConversion function below.
+ */
+function timeConversion(s) {
+    let time = parseInt(s.substring(0, 3));
+    let newTime = time + 12;
+    
+    let result = (newTime + s.substring(2)).slice(0,-2);
+
+    return result;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    let result = timeConversion(s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
